370eefd05b4881753cea5bd59b3bf10380d985cc
Nabarun <pauln@PUNC02R95E9G8WP.local>
2018-01-22 17:33:48 +0530

    linked list implementation

--- src/linkedList.js
+++ src/linkedList.js
@@ -4,12 +4,42 @@ const LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let _node = Node(value);
+    if(list.head){
+      list.tail.next = _node;
+      list.tail = list.tail.next;
+    } else {
+      list.head = _node;
+      list.tail = _node;
+      list.head.next = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    let ret = list.head.value;
+    let temp = list.head.next;
+    delete list.head;
+    list.head = temp;
+    return ret;
   };
 
   list.contains = function(target) {
+    let find = function(node, target) {
+      if(node){
+        if(node.value === target){
+          return true;
+        } 
+        else{
+          return find(node.next, target);
+        }
+      }
+      else {
+        return false;
+      }
+      
+    }
+
+    return find(list.head, target);
   };
 
   return list;
